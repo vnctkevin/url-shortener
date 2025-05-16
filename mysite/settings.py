5ec7f56dd1ea2c3c9198c15721f202d800ab531b
kevin01 <vnctkevin@gmail.com>
2025-05-16 23:25:23 +0700

    fix requirements and settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["vnctkevin.id", "vnctkevin.com", "links.vnctkevin.me", "django-server-production-7928.up.railway.app", ".localhost", "127.0.0.1"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
